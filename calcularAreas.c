7d2bb882d36bdb21ed56504df0cb9e27dad9d177
bruna.farias <bruna.farias@emailcom>
2018-08-08 20:57:41 -0400

    atualizacao do arquivo principal

---
+++ calcularAreas.c
@@ -0,0 +1,11 @@
+/*Oficina Git - Controle de Versão
+	Professor: Orlewilson B. Maia
+	Equipe: Bruna Tatiane, Wendel Nascimento, Natanel Cruz
+	Data: 08/08/2018
+	Descrição: Calcular area do circulo tringulo equilatero e quadrado */
+
+#include <stdio.h>
+int main(){
+	printf("Hello World");
+	return 0;
+}
