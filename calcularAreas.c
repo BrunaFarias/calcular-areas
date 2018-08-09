1df08a3af58b97ebe16fb14c44292ddc9c855d57
bruna.farias <bruna.farias@emailcom>
2018-08-08 21:13:49 -0400

    areas do triangulo e quadrado

--- calcularAreas.c
+++ calcularAreas.c
@@ -5,7 +5,31 @@
 	Descrição: Calcular area do circulo tringulo equilatero e quadrado */
 
 #include <stdio.h>
+
+void areaTriangulo(){
+	int base, altura;
+	float area;
+
+	printf(" insira a base do tringulo");
+	scanf("%d", &base);
+	printf(" insira a altura do tringulo");
+	scanf("%d", &altura);
+
+	area = ((base * altura)/2);
+	//printf("A area do tringulo eh: %d", area );
+		
+}
+
+void areaQuadrado(){
+	int baseQ, alturaQ,areaQ;
+	printf(" insira a base do tringulo");
+	scanf("%d", &baseQ);
+	printf(" insira a altura do tringulo");
+	scanf("%d", &alturaQ);
+
+	area = baseQ * alturaQ;
+}
 int main(){
-	printf("Hello World");
-	return 0;
+	
+
 }
